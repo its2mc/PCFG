8d53de8f85517d404e565d788c72bccd4dfde066
jihoonpar <jihoonpark@mensakorea.org>
2016-11-21 21:29:13 +0900

    Update filter.vhd

--- filter.vhd
+++ filter.vhd
@@ -3,6 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity filter is
     Port ( clk : in  STD_LOGIC;
+			  reset : in STD_LOGIC;
            din : in  STD_LOGIC_VECTOR (7 downto 0);
            dout : out  STD_LOGIC_VECTOR (7 downto 0));
 
@@ -10,7 +11,6 @@ architecture Behavioral of filter is
 
 	component dff
 		Port (  d_clk : in std_logic;
-				  d_reset : in std_logic;
 				  d : in std_logic_vector(7 downto 0);
 				  q : out std_logic_vector(7 downto 0)
 				 );
@@ -40,91 +40,91 @@ begin
 
 	d0 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => din,
 				  q => dout0);
 				  
 	d1 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout0,
 				  q => dout1);
 				  
 	d2 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout1,
 				  q => dout2);
 				  
 	d3 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout2,
 				  q => dout3);
 				  
 	d4 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout3,
 				  q => dout4);
 				  
 	d5 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout4,
 				  q => dout5);
 				  
 	d6 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout5,
 				  q => dout6);
 				  
 	d7 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout6,
 				  q => dout7);
 				  
 	d8 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout7,
 				  q => dout8);
 				  
 	d9 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout8,
 				  q => dout9);
 				  
 	d10 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout9,
 				  q => dout10);
 				  
 	d11 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout10,
 				  q => dout11);
 				  
 	d12 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout11,
 				  q => dout12);
 				  
 	d13 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout12,
 				  q => dout13);
 				  
 	d14 : dff
 	port map ( d_clk => clk,
-				  d_reset => f_reset,
+				  d_reset => reset,
 				  d => dout13,
 				  q => dout14);
 	
