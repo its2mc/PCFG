cde8dd98cc42afc4d17a6d8a45682bde94c35d27
jihoonpar <jihoonpark@mensakorea.org>
2016-11-21 21:28:36 +0900

    Update dff.vhd

--- dff.vhd
+++ dff.vhd
@@ -2,9 +2,10 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity dff is
-    Port ( din : in  STD_LOGIC_VECTOR (7 downto 0);
-           dout : out  STD_LOGIC_VECTOR (7 downto 0);
-           clk : in  STD_LOGIC);
+    Port ( d : in  STD_LOGIC_VECTOR (7 downto 0);
+           q : out  STD_LOGIC_VECTOR (7 downto 0);
+           d_clk : in  STD_LOGIC
+			  d_reset : in STD_LOGIC);
 end dff;
 
 architecture Behavioral of dff is
